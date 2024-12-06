86681cf33fd934b2be5ed4d7dd3bb5390af71bde
RatisWu <61738702+RatisWu@users.noreply.github.com>
2024-12-06 11:39:57 +0800

    p

--- Association/Roads.py
+++ Association/Roads.py
@@ -1,11 +1,11 @@
 import os, sys
-machine_IP_table = r"C:\ExpMachineIP\MachineIP_rec.toml"
-IP_port_table = r"C:\ExpMachineIP\MachinePort_rec.toml"
+machine_IP_table = "/home/UQMS/Server/ExpMachineIP/MachineIP_rec.toml"
 
-queue_folder = r"C:\ExpQueue"
-data_folder = r"C:\ExpData"
+queue_folder = "/home/UQMS/Server/ExpQueue"
+data_folder = "/home/UQMS/Server/ExpData"
 user_dep_config_folder = os.path.join(os.path.expanduser("~"),"MeasConfigs")
 
 for folder in [queue_folder,data_folder,user_dep_config_folder]:
     if not os.path.exists(folder):
         os.mkdir(folder)
+
